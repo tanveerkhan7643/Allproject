ed00d366e0eb8cb9346ace26168e37e136be2437
tanveer khan <tk099936@gmail.com>
2025-10-09 04:50:33 -0800

    Updated some files / added new code

---
+++ Wlaking the DOM/dom1.js
@@ -0,0 +1,34 @@
+
+// let span = document.getElementById('red');
+// span.style.backgroundColor = "red"
+// span.style.color = "white"
+
+// let query = document.querySelector('.item');
+// query.style.background = "red";
+
+// let tag = document.getElementsByTagName('div');
+// for(i = 0; i < tag.length; i++) {
+//     tag[i].style.backgroundColor = "red";
+// }
+
+// let tag = document.getElementsByTagName("div");
+// Array.from(tag).forEach(el => {
+//     el.style.backgroundColor = "pink";
+// });
+
+// let tag = document.getElementsByClassName('item');
+// tag.forEach(el => {
+//     el.style.backgroundcolor = "red";
+// });
+
+// let firstPara = document.querySelector('p');
+// let nextSibling = firstPara.nextElementSibling;  // second paragraph
+// let previousSibling = nextSibling.previousElementSibling; // back to first paragraph
+
+// let p = document.querySelector("p");
+// console.log(p.previousSibling);        // yeh comment node dega
+// console.log(p.previousElementSibling); // yeh <div> dega
+
+// if (parent.contains(item)) {
+//   console.log('Yes, parent contains child');
+// }
